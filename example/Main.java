35bb3d858ed0ca38110dc685adad875859ad969b
Vignesh <vickyprakasy20@gmail.com>
2023-10-27 10:18:29 +0530

    added channel name

--- example/Main.java
+++ example/Main.java
@@ -4,20 +4,21 @@ import java.sql.*;
 import static org.example.Constants.*;
 
 public class Main {
-//    public void addProduct(int product_id, String name, int price, int quantity) {
-//        try (Connection conn = DriverManager.getConnection(url, user, password)) {
-//            String insertQuery = "INSERT INTO company.product (product_id, name, price, quantity) VALUES (?,?, ?, ?)";
-//            try (PreparedStatement preparedStatement = conn.prepareStatement(insertQuery)) {
-//                preparedStatement.setInt(1, product_id);
-//                preparedStatement.setString(2, name);
-//                preparedStatement.setInt(3, price);
-//                preparedStatement.setInt(4, quantity);
-//                preparedStatement.executeUpdate();
-//            }
-//        } catch (SQLException e) {
-//            e.printStackTrace();
-//        }
-//    }
+    public void addProduct(int product_id, String name, int price, int quantity) {
+        try (Connection conn = DriverManager.getConnection(url, user, password)) {
+            String insertQuery = "INSERT INTO company.product (product_id, name, price, quantity) VALUES (?,?, ?, ?)";
+            try (PreparedStatement preparedStatement = conn.prepareStatement(insertQuery)) {
+                preparedStatement.setInt(1, product_id);
+                preparedStatement.setString(2, name);
+                preparedStatement.setInt(3, price);
+                preparedStatement.setInt(4, quantity);
+                preparedStatement.executeUpdate();
+        System.out.println("add succesfully");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
     public void viewProducts() {
         try (Connection conn = DriverManager.getConnection(url, user, password)) {
             String selectQuery = "SELECT * FROM company.Product";
@@ -36,13 +37,40 @@ public class Main {
         }
     }
 
+    public void updateProduct(int productId, String name, int Price, int Quantity) {
+        try (Connection conn = DriverManager.getConnection(url, user, password)) {
+            String updateQuery = "UPDATE company.Product SET Name = ?, Price = ?, Quantity = ? WHERE Product_id = ?";
+            try (PreparedStatement preparedStatement = conn.prepareStatement(updateQuery)) {
+                preparedStatement.setInt(1, productId);
+                preparedStatement.setString(2, name);
+                preparedStatement.setDouble(3, Price);
+                preparedStatement.setInt(4, Quantity);
+                preparedStatement.executeUpdate();
+                System.out.println("Product updated successfully.");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    public void deleteProduct(int productId) {
+        try (Connection conn = DriverManager.getConnection(url, user, password)) {
+            String deleteQuery = "DELETE FROM company.Product WHERE product_id = ?";
+            try (PreparedStatement preparedStatement = conn.prepareStatement(deleteQuery)) {
+                preparedStatement.setInt(1, productId);
+                preparedStatement.executeUpdate();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
 
     public static void main(String[] args) {
         Main main = new Main();
         // Test the addProduct method
-//        main.addProduct(7, "chocolate",10,400 );
+//        main.addProduct(8, "chocolate",10,500 );
         main.viewProducts();
-        System.out.println("add succesfully");
-
+//          main.updateProduct(7, "new_chocolate", 12, 450);
+          main.deleteProduct(8);
     }
 }
